d8b8b25f7faa73c12ff8fe169495ed1608a85c7c
hyeji-ko <7410ajs@gmail.com>
2025-08-24 18:23:21 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyDQEmosR2YJHPQmBMgmFu5hXgQuoGr01Mw",
   authDomain: "study-4cc66.firebaseapp.com",
   projectId: "study-4cc66",
-  storageBucket: "study-4cc66.appspot.com",
+  storageBucket: "study-4cc66.firebasestorage.app",
   messagingSenderId: "663288812068",
   appId: "1:663288812068:web:d8a34f4e20ecf654f2beb1"
 };
