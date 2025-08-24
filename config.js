a50f5cd5687e3ef33efea4009ec06fdd91d8a085
hyeji-ko <7410ajs@gmail.com>
2025-08-24 17:07:59 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,13 @@ window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyDQEmosR2YJHPQmBMgmFu5hXgQuoGr01Mw",
   authDomain: "study-4cc66.firebaseapp.com",
   projectId: "study-4cc66",
-  storageBucket: "study-4cc66.appspot.com",
+  storageBucket: "study-4cc66.firebasestorage.app",
   messagingSenderId: "663288812068",
-  appId: "1:663288812068:web:d8a34f4e20ecf654f2beb1"
+  appId: "1:663288812068:web:d8a34f4e20ecf654f2beb1",
+  measurementId: "G-CFB3X6Q57B"
 };
 // Back-compat alias if needed
 window.firebaseConfig = window.FIREBASE_CONFIG;
+
 
 
